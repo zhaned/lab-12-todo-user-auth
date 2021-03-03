72e31af8bbc27876045cafbe2362ac1fd3ef1adf
zhaned <zhanedmond@gmail.com>
2021-03-03 02:20:43 -0800

    added get post put

---
+++ data/todos.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    todo: 'wash the dishes',
+    completed: false,
+  },
+  {
+    todo: 'do laundry',
+    completed: false,
+  },
+  {
+    todo: 'buy groceries',
+    completed: false,
+  },
+  {
+    todo: 'water garden',
+    completed: false,
+  },
+  {
+    todo: 'feed pet',
+    completed: false,
+  },
+];
